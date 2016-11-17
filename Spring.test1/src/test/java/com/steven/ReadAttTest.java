2d304d6620002b9cb811e528529efaff9bb6c817
Steven <719527102@qq.com>
2016-11-17 15:57:45 +0800

    pom写死版本号，修改system.properties,增加测试用资源，

--- Spring.test1/src/test/java/com/steven/ReadAttTest.java
+++ Spring.test1/src/test/java/com/steven/ReadAttTest.java
@@ -3,9 +3,11 @@ package com.steven;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.support.ConfigurableConversionService;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.Spring.testController1;
 import com.steven.testSpring.test1.dependencyJar.IApp;
 import com.steven.testSpring.test1.dependencyJar.aspectJbean.LogAudience;
 
@@ -17,9 +19,15 @@ public class ReadAttTest {
 	private IApp m_App;
 	@Autowired
 	private LogAudience c_LogAudience;
+	@Autowired
+	private testController1 c_TestController1;
 	@Test
 	public void TestReadA(){
 		m_App.setAtt("dd");
-		org.junit.Assert.assertEquals(m_App.getAtt()+"a", c_LogAudience.getAtt());
+		org.junit.Assert.assertEquals(m_App.getAtt(), c_LogAudience.getAtt());
+	}
+	@Test
+	public void TestResources(){
+		org.junit.Assert.assertEquals("asdg", c_TestController1.testValue());
 	}
 }
