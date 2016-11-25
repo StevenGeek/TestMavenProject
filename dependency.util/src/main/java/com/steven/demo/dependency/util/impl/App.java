92b5fba3072f446ca6d9139fa77e076bc44d7837
Steven <719527102@qq.com>
2016-11-25 14:41:28 +0800

    再次调整名字

---
+++ dependency.util/src/main/java/com/steven/demo/dependency/util/impl/App.java
@@ -0,0 +1,59 @@
+package com.steven.demo.dependency.util.impl;
+
+import com.steven.demo.dependency.util.IApp;
+
+import com.steven.demo.dependency.util.bean.Student;
+import com.steven.demo.dependency.util.bean.Teacher;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App implements IApp
+{
+	public App(){
+		
+	}
+	private String X;
+	private Student student;
+	private Teacher teacher;
+	private String attribute;
+    public String getX() {
+		return X;
+	}
+	public void setX(String x) {
+		X = x;
+	}
+	public Teacher getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	public String testString(){
+//		System.out.println("run testString method......\r\n");
+		String aString = "b";
+    	return aString;
+    }
+	public int testInjectAop(int a){
+		System.out.println("run testInjectAop method......\r\n");
+    	return 2/a;
+    }
+	public void setAtt(String p_Attibute) {
+		this.attribute = p_Attibute;
+	}
+	public String getAtt() {
+		return this.attribute;
+	}
+	public String repositoryTest(){
+		return "see";
+	}
+}
